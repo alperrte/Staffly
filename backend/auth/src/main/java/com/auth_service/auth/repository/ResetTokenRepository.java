78fd5f2864ab187f1f10891b47279f90be19667e
alperrte <alpertemiz1907@hotmail.com>
2026-03-13 14:39:46 +0300

    Repository layers created

---
+++ backend/auth/src/main/java/com/auth_service/auth/repository/ResetTokenRepository.java
@@ -0,0 +1,13 @@
+package com.auth_service.auth.repository;
+
+import com.auth_service.auth.entity.ResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ResetTokenRepository extends JpaRepository<ResetToken, Long> {
+
+    Optional<ResetToken> findByToken(String token);
+}
