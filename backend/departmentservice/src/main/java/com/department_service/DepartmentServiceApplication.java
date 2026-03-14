3a97bf2f964cb92c98ee3f817ca2f58dcc7b6a0f
Nisa Camcı <nscmc34@gmail.com>
2026-03-15 02:40:04 +0300

    Migrations have been created.

--- backend/departmentservice/src/main/java/com/department_service/DepartmentServiceApplication.java
+++ backend/departmentservice/src/main/java/com/department_service/DepartmentServiceApplication.java
@@ -1,5 +1,6 @@
 package com.department_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DepartmentServiceApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        System.setProperty("SERVER_PORT_DEPARTMENT", dotenv.get("SERVER_PORT_DEPARTMENT"));
+        System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+        System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+        System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
         SpringApplication.run(DepartmentServiceApplication.class, args);
     }
-
 }
--- backend/departmentservice/src/main/resources/db/migration/V20__create_departments_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE department.departments (
-                                        id BIGINT IDENTITY(1,1) PRIMARY KEY,
-                                        name NVARCHAR(255) NOT NULL,
-                                        description NVARCHAR(500),
-                                        manager_id BIGINT,
-                                        created_at DATETIME2 DEFAULT GETDATE(),
-                                        updated_at DATETIME2,
-                                        deleted BIT DEFAULT 0
-);
