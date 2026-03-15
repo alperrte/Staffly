907647c019ad338f9452796219da2ca72ca071da
alperrte <alpertemiz1907@hotmail.com>
2026-03-15 22:42:03 +0300

    Frontend has been created. Login page is ready.

--- backend/auth/src/main/java/com/auth_service/auth/config/SecurityConfig.java
+++ backend/auth/src/main/java/com/auth_service/auth/config/SecurityConfig.java
@@ -74,4 +74,21 @@ public class SecurityConfig {
 
         return http.build();
     }
+    @Bean
+    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+
+        org.springframework.web.cors.CorsConfiguration config = new org.springframework.web.cors.CorsConfiguration();
+
+        config.setAllowedOriginPatterns(java.util.List.of("http://localhost:*"));
+        config.setAllowedMethods(java.util.List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(java.util.List.of("*"));
+        config.setAllowCredentials(true);
+
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source =
+                new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 }
