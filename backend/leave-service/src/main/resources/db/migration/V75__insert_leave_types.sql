57c16aaa547d954b37394ee6a6016b35cd4ebb04
Nisa Camcı <nscmc34@gmail.com>
2026-05-02 18:21:44 +0300

    leave management system backend completed

---
+++ backend/leave-service/src/main/resources/db/migration/V75__insert_leave_types.sql
@@ -0,0 +1,10 @@
+INSERT INTO leave.leave_types (name, description, is_hourly) VALUES
+                                                                 (N'YILLIK_IZIN', N'Yıllık izin', 0),
+                                                                 (N'HASTALIK_IZNI', N'Hastalık izni', 0),
+                                                                 (N'MAZERET_IZNI', N'Mazeret izni', 0),
+                                                                 (N'UCRETSIZ_IZIN', N'Ücretsiz izin', 0),
+                                                                 (N'DOGUM_IZNI', N'Doğum izni', 0),
+                                                                 (N'BABALIK_IZNI', N'Babalık izni', 0),
+                                                                 (N'EVLILIK_IZNI', N'Evlilik izni', 0),
+                                                                 (N'VEFAT_IZNI', N'Vefat izni', 0),
+                                                                 (N'YARIM_GUN_IZIN', N'Yarım gün izin', 1);
